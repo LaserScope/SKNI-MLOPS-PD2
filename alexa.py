9b2b6ae52b6460ff34a1c51897d44c9a1fd53fd0
LaserScope <122286194+LaserScope@users.noreply.github.com>
2023-01-13 19:34:31 +0100

    Update alexa.py

--- alexa.py
+++ alexa.py
@@ -1,10 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
-from requests_html import HTMLSession
-session = HTMLSession()
 import time
 import datetime
-import os
 
 URL = 'https://www.amazon.pl/zupelnie-nowe-echo-dot-4-generacji-wersja-miedzynarodowa/dp/B085K45C3S/ref=lp_22832478031_1_3'
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36", "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
@@ -13,12 +10,14 @@ page = requests.get(URL, headers=headers)
 soup = BeautifulSoup(page.content, "lxml")
 
 
+title = soup.select_one("span[id='productTitle']").text
 price = soup.select_one("span[class='a-offscreen']").text
 rating = soup.select_one("span[class='a-icon-alt']").text
 brand = soup.find(id='bylineInfo')
 
 title = title.strip() [:52]
 
+print(title)
 print(price)
 print(rating)
 print(brand.text)

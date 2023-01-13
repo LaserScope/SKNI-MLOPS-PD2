519f583d92445b2866ca2f6a49c4739b2bb42da2
LaserScope <122286194+LaserScope@users.noreply.github.com>
2023-01-13 20:49:05 +0100

    Update alexa.py

--- alexa.py
+++ alexa.py
@@ -2,18 +2,20 @@ from bs4 import BeautifulSoup
 import requests
 import time
 import datetime
-import os
 
 URL = 'https://www.amazon.pl/zupelnie-nowe-echo-dot-4-generacji-wersja-miedzynarodowa/dp/B085K45C3S/ref=lp_22832478031_1_3'
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36", "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
+headers = ({'User-Agent':
+            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+            'Accept-Language': 'en-US, en;q=0.5'})
 
 page = requests.get(URL, headers=headers)
 soup = BeautifulSoup(page.content, "lxml")
 
-brand = soup.find(id='bylineInfo').get_text()
+title = soup.find("span", attrs={"id":'productTitle'})
+title_value = title.string
+title_string = title_value.strip()
 
-
-print(brand)
+print(title_string)
 
 import datetime
 download_date = datetime.date.today()

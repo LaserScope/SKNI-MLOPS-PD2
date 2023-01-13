b15c67b3e12ce9cc0ac4b5ad8c959da88de82116
LaserScope <122286194+LaserScope@users.noreply.github.com>
2023-01-13 22:16:48 +0100

    Update alexa.py

--- alexa.py
+++ alexa.py
@@ -17,8 +17,6 @@ def get_title(soup):
 		title = soup.find("span", attrs={"id":'productTitle'})
 		title_value = title.string
 		title_string = title_value.strip()
-		
-	print(get_title(soup))
 	
 	except AttributeError:
 		title_string = ""
@@ -30,8 +28,6 @@ def get_price(soup):
 	try:
 		price = soup.find("span", attrs={'id':'priceblock_ourprice'}).string.strip()
 		
-	print(get_price(soup))
-
 	except AttributeError:
 		price = ""
 
@@ -42,7 +38,6 @@ def get_rating(soup):
 	try:
 		rating = soup.find("i", attrs={'class':'a-icon a-icon-star a-star-4-5'}).string.strip()
 		
-	print(get_rating(soup))
 		
 	except AttributeError:
 		
@@ -58,14 +53,16 @@ def get_review_count(soup):
 	try:
 		review_count = soup.find("span", attrs={'id':'acrCustomerReviewText'}).string.strip()
 	
-	print(get_review_count(soup))
-		
 	except AttributeError:
 		review_count = ""
 
 	return review_count
 
-
+print(get_title(soup))
+print(get_price(soup))
+print(get_rating(soup))
+print(get_review_count(soup))
+		
 import datetime
 download_date = datetime.date.today()
 print(download_date)
